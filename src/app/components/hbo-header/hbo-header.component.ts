d3519531c770054ff5891fdcf0cfae7c585f48a7
WilmerRS <est.wilmerrodriguez@gmail.com>
2023-04-06 11:48:53 -0500

    feat: add header component

---
+++ src/app/components/hbo-header/hbo-header.component.ts
@@ -0,0 +1,39 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hbo-header',
+  templateUrl: './hbo-header.component.html',
+  styleUrls: ['./hbo-header.component.css'],
+})
+export class HboHeaderComponent implements OnInit {
+  isScrollInTop: boolean = true;
+
+  ngOnInit(): void {
+    this._validateIfScrollIsInTop();
+  }
+
+  @HostListener('window:scroll', [])
+  private _onWindowScroll(): void {
+    this._validateIfScrollIsInTop();
+  }
+
+  private _validateIfScrollIsInTop(): void {
+    const verticalOffset = this._getScrollYPosition();
+
+    const scrollIsLessThanHeader = verticalOffset < 50
+    if (scrollIsLessThanHeader === this.isScrollInTop) {
+      return
+    }
+
+    this.isScrollInTop = scrollIsLessThanHeader;
+  }
+
+  private _getScrollYPosition(): number {
+    return (
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0
+    );
+  }
+}
