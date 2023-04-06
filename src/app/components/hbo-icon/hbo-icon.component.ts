9d12688bac856890c4df55b1a259a8c29ecbc6ab
WilmerRS <est.wilmerrodriguez@gmail.com>
2023-04-06 11:49:02 -0500

    feat: add icon component

---
+++ src/app/components/hbo-icon/hbo-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hbo-icon',
+  templateUrl: './hbo-icon.component.html',
+  styleUrls: ['./hbo-icon.component.css']
+})
+export class HboIconComponent {
+
+}
