69dceae529e7bf449f9028886f91020ff89efac7
WilmerRS <est.wilmerrodriguez@gmail.com>
2023-04-07 11:21:10 -0500

    feat: add footer

---
+++ src/app/components/hbo-footer/hbo-footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hbo-footer',
+  templateUrl: './hbo-footer.component.html',
+  styleUrls: ['./hbo-footer.component.css'],
+})
+export class HboFooterComponent {}
