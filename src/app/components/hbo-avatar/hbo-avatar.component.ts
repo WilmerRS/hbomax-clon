1f2ebd4fd88b51d1ed7a3e2b1d99dd5ca07cbd9d
WilmerRS <est.wilmerrodriguez@gmail.com>
2023-04-07 09:14:35 -0500

    feat: add avatar component

---
+++ src/app/components/hbo-avatar/hbo-avatar.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'hbo-avatar',
+  templateUrl: './hbo-avatar.component.html',
+  styleUrls: ['./hbo-avatar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HboAvatarComponent {
+
+}
