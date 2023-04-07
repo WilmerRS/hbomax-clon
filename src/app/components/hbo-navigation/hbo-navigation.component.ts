b08e4276a03656eba4203b4d4cc20e4c3f821959
WilmerRS <est.wilmerrodriguez@gmail.com>
2023-04-07 09:14:30 -0500

    feat: add avatar component

---
+++ src/app/components/hbo-navigation/hbo-navigation.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'hbo-navigation',
+  templateUrl: './hbo-navigation.component.html',
+  styleUrls: ['./hbo-navigation.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HboNavigationComponent {
+
+}
